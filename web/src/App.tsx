1b0013a905b97e437060a8ca263ce239ae2706a3
sm231219933 <sm231219933@gmail.com>
2026-09-15 15:45:18 +0530

    Update web API tunnel URL

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -16,7 +16,7 @@ type User = {
 
 type AuthResponse = { token: string; user: User }
 
-const API_BASE = (import.meta.env.VITE_API_BASE_URL || 'https://divx-coding-discretion-wise.trycloudflare.com').replace(/\/$/, '')
+const API_BASE = (import.meta.env.VITE_API_BASE_URL || 'https://assembled-warriors-traveler-smaller.trycloudflare.com').replace(/\/$/, '')
 
 async function api<T>(path: string, options: RequestInit = {}): Promise<T> {
   const token = localStorage.getItem('tinklet_token')
