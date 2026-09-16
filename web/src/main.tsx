4fd6c4e5525de15b2b5bc30b6d63a441a61b7329
sm231219933 <sm231219933@gmail.com>
2026-09-16 15:33:08 +0530

    Load Android-aligned web enhancements

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import WebEnhancements from './WebEnhancements'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <WebEnhancements />
   </React.StrictMode>
 )
